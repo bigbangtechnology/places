383e095c7641e4a2906f845b4ec172604b08b272
Adam Doeler <adam@adamdoeler.com>
2011-06-15 14:39:16 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Places::Application.config.secret_token = '53801765dd60fe54a1bc2f0145042fafb48ad84c6e29caf8d5a96e4594e0f24f107081cee1f5e816b24432eca3c3383102dad57e23becb783b760375b75974d1'
